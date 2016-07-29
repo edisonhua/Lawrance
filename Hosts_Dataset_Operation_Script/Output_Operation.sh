e6e9a2bbf3826f2a3e3022a26231cd4eee429284
Mai Hua <Edison@62.196.128.198.in-addr.dhcp.lbnl.us>
2016-07-29 11:48:57 -0700

    new for Hosts_Dataset

--- Hosts_Dataset_Operation_Script/Output_Operation.sh
+++ Hosts_Dataset_Operation_Script/Output_Operation.sh
@@ -1,6 +1,7 @@
 awk '!($1 in a){a[$1];print}' < last-output.txt | awk '{print $1}' > namelist
 
 name_to_exclude=(sdou root sjames kaisong yqin fernsler flexlm)
+now="lastlogin-$(date +"%Y%m%d").txt"
 
 awk '$4 == "Jan" {print $0}' < last-output.txt | sort -u -k1 > Jan.txt
 awk '$4 == "Feb" {print $0}' < last-output.txt | sort -u -k1 > Feb.txt
@@ -27,7 +28,9 @@ awk -F'[ +:()]+' 'FNR==NR{a[$1]; next;} !($1 in a){next} NF==13{b[$1]+=$12/60+$1
 awk -F'[ +:()]+' 'FNR==NR{a[$1]; next;} !($1 in a){next} NF==13{b[$1]+=$12/60+$11} NF==14{b[$1]+=$13/60+$12+24*$11} END{print "[Sep]"; for (n in b)print n,b[n],"hours"; print "\n"}' namelist Sep.txt > t9.txt
 awk -F'[ +:()]+' 'FNR==NR{a[$1]; next;} !($1 in a){next} NF==13{b[$1]+=$12/60+$11} NF==14{b[$1]+=$13/60+$12+24*$11} END{print "[Oct]"; for (n in b)print n,b[n],"hours"; print "\n"}' namelist Oct.txt > t10.txt
 awk -F'[ +:()]+' 'FNR==NR{a[$1]; next;} !($1 in a){next} NF==13{b[$1]+=$12/60+$11} NF==14{b[$1]+=$13/60+$12+24*$11} END{print "[Nov]"; for (n in b)print n,b[n],"hours"; print "\n"}' namelist Nov.txt > t11.txt
+awk -F'[ +:()]+' 'FNR==NR{a[$1]; next;} !($1 in a){next} NF==13{b[$1]+=$12/60+$11} NF==14{b[$1]+=$13/60+$12+24*$11} END{print "[TOTALS]"; for (n in b)print n,b[n],"hours"; print "\n"}' namelist last-output.txt > 1.txt
 
+(IFS="|"; grep -vE "(${name_to_exclude[*]})" 1.txt > 1_new.txt)
 (IFS="|"; grep -vE "(${name_to_exclude[*]})" t12.txt > t12_new.txt)
 (IFS="|"; grep -vE "(${name_to_exclude[*]})" t1.txt > t1_new.txt)
 (IFS="|"; grep -vE "(${name_to_exclude[*]})" t2.txt > t2_new.txt)
@@ -41,12 +44,13 @@ awk -F'[ +:()]+' 'FNR==NR{a[$1]; next;} !($1 in a){next} NF==13{b[$1]+=$12/60+$1
 (IFS="|"; grep -vE "(${name_to_exclude[*]})" t10.txt > t10_new.txt)
 (IFS="|"; grep -vE "(${name_to_exclude[*]})" t11.txt > t11_new.txt)
 
+awk '{total = total + $2} END {print "[GRAND TOTAL TIME]"; print total, "hours"}' 1_new.txt > grandtotal.txt
 
+cat t12_new.txt t1_new.txt t2_new.txt t3_new.txt t4_new.txt t5_new.txt t6_new.txt t7_new.txt t8_new.txt t9_new.txt t10_new.txt t11_new.txt 1_new.txt grandtotal.txt > "$now"
 
-cat t12_new.txt t1_new.txt t2_new.txt t3_new.txt t4_new.txt t5_new.txt t6_new.txt t7_new.txt t8_new.txt t9_new.txt t10_new.txt t11_new.txt > output.txt
-
+rm namelist
 rm Jan.txt Feb.txt Mar.txt Apr.txt May.txt Jun.txt Jul.txt Aug.txt Sep.txt Oct.txt Nov.txt Dec.txt
-rm t1.txt t2.txt t3.txt t4.txt t5.txt t6.txt t7.txt t8.txt t9.txt t10.txt t11.txt t12.txt
-rm t1_new.txt t2_new.txt t3_new.txt t4_new.txt t5_new.txt t6_new.txt t7_new.txt t8_new.txt t9_new.txt t10_new.txt t11_new.txt t12_new.txt
+rm t1.txt t2.txt t3.txt t4.txt t5.txt t6.txt t7.txt t8.txt t9.txt t10.txt t11.txt t12.txt 1.txt
+rm t1_new.txt t2_new.txt t3_new.txt t4_new.txt t5_new.txt t6_new.txt t7_new.txt t8_new.txt t9_new.txt t10_new.txt t11_new.txt t12_new.txt 1_new.txt grandtotal.txt
 
 
